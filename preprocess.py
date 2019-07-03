ad021e98d2c923018c84433e911a8b53ab2152e7
ferrari4444 <petopulik@gmail.com>
2019-07-03 17:14:09 +0200

    Partially fixed different file formats

--- preprocess.py
+++ preprocess.py
@@ -1,5 +1,9 @@
+import os
 import sys
+import csv
+import imghdr
 import requests
+from shutil import move
 from datetime import datetime
 from dateutil.relativedelta import relativedelta
 
@@ -40,9 +44,9 @@ if len(sys.argv) != 2 and len(sys.argv) != 3:
     sys.exit(1)
 
 if len(sys.argv) == 3:
-    csv = open(sys.argv[2], "w")
+    csv_output = open(sys.argv[2], "w")
 else:
-    csv = open("students.csv", "w")
+    csv_output = open("students.csv", "w")
 
 total_lines = 0
 
@@ -51,16 +55,21 @@ with open(sys.argv[1], "r") as f:
         total_lines += 1
 
 with open(sys.argv[1], "r") as f:
-    for line in f:
+    csv_reader = csv.reader(f, delimiter=',')
+    dirName = 'pictures'
+    try:
+        # Create target Directory
+        os.mkdir(dirName)
+        print("Directory " + dirName +  " Created ") 
+    except:
+        print("Directory " + dirName +  " already exists")
+    for line in csv_reader:
         if line_number == 0:
-            #print("name,country,D0,D1,M0,M1,Y0,Y1")
-            csv.write("name,country,D0,D1,M0,M1,Y0,Y1,")
-            csv.write("TD0,TD1,TM0,TM1,TY0,TY1\n")
+            csv_output.write("name,country,D0,D1,M0,M1,Y0,Y1,")
+            csv_output.write("TD0,TD1,TM0,TM1,TY0,TY1\n")
         else:
-            #print line
             print(str(line_number) + "/" + str(total_lines - 1))
-            values = line.split(",")
-            name = values[1][1:-1]
+            name = line[1]
             if len(name) > 30:
                 print('WARNING: Name "' + name + '" longer than 30 characters!')
                 names = name.split()
@@ -75,8 +84,8 @@ with open(sys.argv[1], "r") as f:
                 for ntu in names_to_use:
                     name += " " + names[int(ntu)]
                 name = name[1:]
-            country = values[2][1:-1]
-            date_str = values[3][1:-1]
+            country = line[2]
+            date_str = line[3]
             date_obj = datetime.strptime(date_str, "%Y-%m-%d").date()
             today_str = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
             age = relativedelta(datetime.now(), date_obj).years
@@ -94,12 +103,15 @@ with open(sys.argv[1], "r") as f:
             TM1 = today_str[6]
             TY0 = today_str[2]
             TY1 = today_str[3]
-            #print(name + "," + country + "," + D0 + "," + D1 + "," + M0 + "," + M1 + "," + Y0 + "," + Y1)
-            csv.write(name + "," + country + "," + D0 + "," + D1 + "," + M0 + "," + M1 + "," + Y0 + "," + Y1 + ",")
-            csv.write(TD0 + "," + TD1 + "," + TM0 + "," + TM1 + "," + TY0 + "," + TY1 + "\n")
-            file_id = values[4][values[4].find("id=") + 3:-1]
-            #print(file_id)
-            download_file_from_google_drive(file_id, name + ".jpg")
+            csv_output.write(name + "," + country + "," + D0 + "," + D1 + "," + M0 + "," + M1 + "," + Y0 + "," + Y1 + ",")
+            csv_output.write(TD0 + "," + TD1 + "," + TM0 + "," + TM1 + "," + TY0 + "," + TY1 + "\n")
+            file_id = line[4][line[4].find("id=") + 3:]
+            download_file_from_google_drive(file_id, name)
+            fileType = imghdr.what(name)
+            if fileType == None:
+                print("Couldn't find out the picture type, please add it manually to the end of the filename for " + name)
+            else:
+                move(name, os.path.join('pictures', name + '.' + fileType))
         line_number += 1
 
-csv.close()
+csv_output.close()
